32a780c9fb484cd56238848730b42e0cb6ce29e1
Lukas Bockstaller <lukas.bockstaller@posteo.de>
2021-12-17 16:12:46 +0100

    adds exporters for DHL and Deutsche Post

--- voco_exporter/exporter.py
+++ voco_exporter/exporter.py
@@ -4,6 +4,7 @@ from django_scopes import scope
 from pretix.base.exporter import ListExporter
 from pretix.base.models.items import Item
 from pretix.base.models.orders import Order, QuestionAnswer
+from enum import Enum
 
 
 class GroupExporter(ListExporter):
@@ -189,3 +190,217 @@ class GroupExporter(ListExporter):
                     nachzu16,
                     nachzl,
                 ]
+
+
+class Sizes(Enum):
+    Maxibrief = "Maxibrief"
+    Paeckchen_S = "PAECKS.DEU"
+    Paeckchen_M = "PAECK.DEU"
+    Packet_2KG = "PAK02.DEU"
+    Packet_5KG = "PAK05.DEU"
+    Packet_10KG = "PAK10.DEU"
+    Packet_31KG = "PAK31.DEU"
+
+
+class DHLExporter(ListExporter):
+    identifier = "voco-dhlexporter"
+    verbose_name = "roverVOCO DHL"
+
+    def get_filename(self):
+        return "{}_dhl".format(self.event.slug)
+
+    def iterate_list(self, form_data):
+        headers = ["Order Code"]
+
+        headers.append("Anzahl Proj.")
+        headers.append("Anzahl Nachz.")
+        headers.append("SEND_NAME1")
+        headers.append("SEND_NAME2")
+        headers.append("SEND_STREET")
+        headers.append("SEND_HOUSENUMBER")
+        headers.append("SEND_PLZ")
+        headers.append("SEND_CITY")
+        headers.append("SEND_COUNTRY")
+        headers.append("RECV_NAME1")
+        headers.append("RECV_NAME2")
+        headers.append("RECV_STREET")
+        headers.append("RECV_HOUSENUMBER")
+        headers.append("RECV_PLZ")
+        headers.append("RECV_CITY")
+        headers.append("RECV_COUNTRY")
+        headers.append("PRODUCT")
+        headers.append("COUPON")
+        headers.append("SEND_EMAIL")
+
+        yield headers
+
+        tn = Item.objects.filter(id__in=[27, 28, 31])
+        nz = Item.objects.filter(id__in=[45, 51, 53])
+        gruppenleiter = Item.objects.filter(id__in=[27, 51])
+
+        data = [
+            ("W7N8FX3M", "RECV_STREET"),
+            ("JGCZN9CQ", "RECV_HOUSENUMBER"),
+            ("NETUFAER", "RECV_PLZ"),
+            ("DXARB8KL", "RECV_CITY"),
+        ]
+
+        with scope(event=self.event):
+
+            os = (
+                Order.objects.exclude(status=Order.STATUS_CANCELED)
+                .filter(event=self.event)
+                .all()
+            )
+            for o in os:
+                proj = 0
+                nachz = 0
+
+                d = {
+                    "SEND_NAME1": "Marvin Anselm",
+                    "SEND_NAME2": "Bundesamt Sankt Georg e.V.",
+                    "SEND_STREET": "Martinstraße",
+                    "SEND_HOUSENUMBER": "2",
+                    "SEND_PLZ": "41472",
+                    "SEND_CITY": "Neuss (Holzheim)",
+                    "SEND_COUNTRY": "DEU",
+                    "RECV_NAME1": "",
+                    "RECV_NAME2": "",
+                    "RECV_STREET": "",
+                    "RECV_HOUSENUMBER": "",
+                    "RECV_PLZ": "",
+                    "RECV_CITY": "",
+                    "RECV_COUNTRY": "DEU",
+                    "PRODUCT": "",
+                    "COUPON": "",
+                    "SEND_EMAIL": "LUKAS.BOCKSTALLER@POSTEO.DE",
+                }
+
+                product, coupon = "", ""
+
+                for p in o.all_positions.all():
+                    if p.canceled:
+                        continue
+
+                    if p.item in gruppenleiter:
+                        d["RECV_NAME1"] = p.attendee_name_cached
+
+                        for key, var in data:
+                            try:
+                                d[var] = p.answers.get(question__identifier=key).answer
+
+                            except QuestionAnswer.DoesNotExist:
+                                pass
+
+                    if p.item in tn:
+                        proj += 1
+
+                    if p.item in nz:
+                        nachz += 1
+
+                if sum([proj, nachz]) > 28:
+                    d["PRODUCT"] = Sizes.Packet_10KG.value
+
+                if sum([proj, nachz]) <= 28:
+                    d["PRODUCT"] = Sizes.Packet_5KG.value
+
+                if sum([proj, nachz]) <= 13:
+                    d["PRODUCT"] = Sizes.Paeckchen_S.value
+
+                if sum([proj, nachz]) <= 6:
+                    d["PRODUCT"] = Sizes.Maxibrief.value
+                    continue
+
+                yield [
+                    o.code,
+                    proj,
+                    nachz,
+                ] + list(d.values())
+
+
+class PostExporter(ListExporter):
+    identifier = "voco-postexporter"
+    verbose_name = "roverVOCO POST"
+
+    def get_filename(self):
+        return "{}_post".format(self.event.slug)
+
+    def iterate_list(self, form_data):
+
+        headers = ["Anzahl Proj."]
+        headers.append("Anzahl Nachz.")
+        headers.append("NAME")
+        headers.append("ZUSATZ")
+        headers.append("STRASSE")
+        headers.append("NUMMER")
+        headers.append("PLZ")
+        headers.append("STADT")
+        headers.append("LAND")
+        headers.append("ADRESS_TYP")
+        headers.append("REFERENZ")
+
+        yield headers
+
+        tn = Item.objects.filter(id__in=[27, 28, 31])
+        nz = Item.objects.filter(id__in=[45, 51, 53])
+        gruppenleiter = Item.objects.filter(id__in=[27, 51])
+
+        data = [
+            ("W7N8FX3M", "STRASSE"),
+            ("JGCZN9CQ", "NUMMER"),
+            ("NETUFAER", "PLZ"),
+            ("DXARB8KL", "STADT"),
+        ]
+
+        with scope(event=self.event):
+
+            os = (
+                Order.objects.exclude(status=Order.STATUS_CANCELED)
+                .filter(event=self.event)
+                .all()
+            )
+            for o in os:
+                proj = 0
+                nachz = 0
+
+                d = {
+                    "NAME": "",
+                    "ZUSATZ": "",
+                    "STRASSE": "",
+                    "NUMMER": "",
+                    "PLZ": "",
+                    "STADT": "",
+                    "LAND": "DEU",
+                    "ADRESS_TYP": "HOUSE",
+                    "REFERENZ": o.code,
+                }
+
+                product, coupon = "", ""
+
+                for p in o.all_positions.all():
+                    if p.canceled:
+                        continue
+
+                    if p.item in gruppenleiter:
+                        d["NAME"] = p.attendee_name_cached
+
+                        for key, var in data:
+                            try:
+                                d[var] = p.answers.get(question__identifier=key).answer
+
+                            except QuestionAnswer.DoesNotExist:
+                                pass
+
+                    if p.item in tn:
+                        proj += 1
+
+                    if p.item in nz:
+                        nachz += 1
+
+                if sum([proj, nachz]) > 6:
+                    continue
+
+                yield [
+                    proj,
+                    nachz,
+                ] + list(d.values())
--- voco_exporter/signals.py
+++ voco_exporter/signals.py
@@ -3,8 +3,22 @@ from django.dispatch import receiver
 from pretix.base.signals import register_data_exporters
 
 
-@receiver(register_data_exporters, dispatch_uid="voco_exporter")
+@receiver(register_data_exporters, dispatch_uid="voco_groupexporter")
 def register_data_exporter(sender, **kwargs):
     from .exporter import GroupExporter
 
     return GroupExporter
+
+
+@receiver(register_data_exporters, dispatch_uid="voco_dhlexporter")
+def register_data_exporter(sender, **kwargs):
+    from .exporter import DHLExporter
+
+    return DHLExporter
+
+
+@receiver(register_data_exporters, dispatch_uid="voco_postexporter")
+def register_data_exporter(sender, **kwargs):
+    from .exporter import PostExporter
+
+    return PostExporter
