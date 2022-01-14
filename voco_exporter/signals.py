0d483046bd99d93c365ce82dcbf5c8be4bdfa660
Lukas Bockstaller <lukas.bockstaller@posteo.de>
2022-01-14 13:58:54 +0100

    fixes duplicate function names

--- voco_exporter/signals.py
+++ voco_exporter/signals.py
@@ -4,21 +4,21 @@ from pretix.base.signals import register_data_exporters
 
 
 @receiver(register_data_exporters, dispatch_uid="voco_groupexporter")
-def register_data_exporter(sender, **kwargs):
+def register_group_exporter(sender, **kwargs):
     from .exporter import GroupExporter
 
     return GroupExporter
 
 
 @receiver(register_data_exporters, dispatch_uid="voco_dhlexporter")
-def register_data_exporter(sender, **kwargs):
+def register_dhl_exporter(sender, **kwargs):
     from .exporter import DHLExporter
 
     return DHLExporter
 
 
 @receiver(register_data_exporters, dispatch_uid="voco_postexporter")
-def register_data_exporter(sender, **kwargs):
+def register_post_exporter(sender, **kwargs):
     from .exporter import PostExporter
 
     return PostExporter
